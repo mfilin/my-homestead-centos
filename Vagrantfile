a0303a2bd0bddbb888e5202b429f030ac087d059
Ricardo <ricardo.pcan@gmail.com>
2015-09-24 20:54:50 -0500

    1.0 release finish

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,5 @@
 Vagrant.configure(2) do |config|
-    config.vm.box = "puppetlabs/centos-7.0-64-puppet"
+    config.vm.box = "centos/7"
     config.vm.provision "file", source:"provision/templates/", destination: "/home/vagrant/provision/templates/"
     config.vm.provision "shell", path: "provision/provision.sh"
     config.vm.provision "shell", path: "provision/nodejs.sh", privileged: false
@@ -9,8 +9,8 @@ Vagrant.configure(2) do |config|
         vb.memory = "2048"
         vb.cpus = 2
     end
-    config.vm.network :forwarded_port, guest: 8000, host: 8000
-    config.vm.network :private_network, ip: "192.168.23.36"
+    config.vm.network :forwarded_port, guest: 80, host: 3000
+    config.vm.network :private_network, ip: "192.168.33.77"
     config.vm.hostname = "www.example.local"
     config.hostsupdater.aliases = ["example.local"]
     config.vm.synced_folder "./code", "/home/vagrant/example", type: "nfs"
--- provision/provision.sh
+++ provision/provision.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-    yum update
+    yum update -y
     yum clean all
 
     # Install epel-release-7
     rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
     rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-    
+
     yum clean all
 
-    PACKAGES="httpd mariadb-server mariadb php56w php56w-opcache php56w-mcrypt php56w-common curl tree vim unzip"
+    PACKAGES="httpd mariadb-server mariadb php56w php56w-opcache php56w-mcrypt php56w-pear php56w-pdo php56w-mbstring php56w-common curl tree vim unzip"
+    PACKAGES="$PACKAGES policycoreutils policycoreutils-python selinux-policy selinux-policy-targeted"
+    PACKAGES="$PACKAGES libselinux-utils setroubleshoot-server setools setools-console mcstrans wget"
 
     debconf-set-selections <<< "mysql-server mysql-server/root_password password password"
     debconf-set-selections <<< "mysql-server mysql-server/root_password_again password password"
@@ -36,6 +38,8 @@
     cp -r laravel-master/* example/
     rm master.zip
     rm -r laravel-master
+    echo "Move .env file"
+    cp -f /home/vagrant/provision/templates/.env.example /home/vagrant/example/.env
 
     echo "Create www folders"
     #create sites httpd folders
@@ -43,9 +47,15 @@
     mkdir /etc/httpd/sites-enabled
     cp -f /home/vagrant/provision/templates/httpd.conf /etc/httpd/conf/httpd.conf
 
+    echo "Move userdir"
+    cp -f /home/vagrant/provision/templates/userdir.conf /etc/httpd/conf.d/userdir.conf
+
     echo "Create apache virtual host"
-    cp /home/vagrant/provision/templates/example.apache /etc/httpd/sites-available/example.conf
-    ln -s /etc/httpd/sites-available/example.conf /etc/httpd/sites-enabled/example.conf
+    cp /home/vagrant/provision/templates/example.apache /etc/httpd/sites-available/example.app.conf
+    ln -s /etc/httpd/sites-available/example.app.conf /etc/httpd/sites-enabled/example.app.conf
     systemctl start httpd.service
+
+    echo "Change SELinux mode"
+    setenforce 0
 
     yum clean all
